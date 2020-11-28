0b775c11fbdb76a69356c0ebb59495d672e1ee70
sndnvaps <sndnvaps@gmail.com>
2020-11-28 21:37:11 +0800

    重新修改代码，使用app.ini定义日志输出模式

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/go-ini/ini"
+	"github.com/henson/proxypool/pkg/util"
 	clog "unknwon.dev/clog/v2"
 )
 
@@ -119,9 +120,81 @@ func NewContext() {
 	SessionExpires = sec.Key("SESSION_EXPIRES").MustDuration(time.Hour * 24 * 7)
 }
 
+//NewLogService Init clog service
 func NewLogService() {
-	err := clog.NewConsole()
-	if err != nil {
-		panic("unable to create new logger: " + err.Error())
+	// Because we always create a console logger as primary logger before all settings are loaded,
+	// thus if user doesn't set console logger, we should remove it after other loggers are created.
+
+	// Get the log mode.
+	if DebugMode {
+		LogModes = strings.Split("console", ",")
+	} else {
+		LogModes = strings.Split(Cfg.Section("log").Key("MODE").MustString("console"), ",")
 	}
+
+	levelNames := map[string]clog.Level{
+		"trace": clog.LevelTrace,
+		"info":  clog.LevelInfo,
+		"warn":  clog.LevelWarn,
+		"error": clog.LevelError,
+		"fatal": clog.LevelFatal,
+	}
+
+	for _, mode := range LogModes {
+		mode = strings.ToLower(strings.TrimSpace(mode))
+		sec, err := Cfg.GetSection("log." + mode)
+		if err != nil {
+			clog.Fatal("Unknown logger mode: %s", mode)
+		}
+
+		validLevels := []string{"trace", "info", "warn", "error", "fatal"}
+		name := Cfg.Section("log." + mode).Key("LEVEL").Validate(func(v string) string {
+			v = strings.ToLower(v)
+			if util.IsSliceContainsStr(validLevels, v) {
+				return v
+			}
+			return "trace"
+		})
+		level := levelNames[name]
+
+		// Generate log configuration.
+		switch mode {
+		case "console":
+			BufferSize := Cfg.Section("log").Key("BUFFER_LEN").MustInt64(100)
+			err = clog.NewConsole(BufferSize,
+				clog.ConsoleConfig{
+					Level: level,
+				},
+			)
+			if err != nil {
+				clog.Warn("unable to create new logger: " + err.Error())
+			}
+
+		case "file":
+			logPath := path.Join(LogRootPath, "ProxyPool.log")
+			if err = os.MkdirAll(path.Dir(logPath), os.ModePerm); err != nil {
+				clog.Warn("Fail to create log directory '%s': %v", path.Dir(logPath), err)
+			}
+
+			err = clog.NewFile(clog.FileConfig{
+				Level:    level,
+				Filename: logPath,
+				FileRotationConfig: clog.FileRotationConfig{
+					Rotate:   sec.Key("LOG_ROTATE").MustBool(true),
+					Daily:    sec.Key("DAILY_ROTATE").MustBool(true),
+					MaxSize:  1 << uint(sec.Key("MAX_SIZE_SHIFT").MustInt(28)),
+					MaxLines: sec.Key("MAX_LINES").MustInt64(1000000),
+					MaxDays:  sec.Key("MAX_DAYS").MustInt64(7),
+				},
+			},
+			)
+
+		}
+
+		clog.Trace("Log Mode: %s (%s)", strings.Title(mode), strings.Title(name))
+	}
+
+	// Make sure everyone gets version info printed.
+	clog.Info("%s %s", AppName, AppVer)
+
 }
