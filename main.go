292ec71df4935ce595dc0edaee43547d507615cb
sndnvaps <sndnvaps@gmail.com>
2020-12-01 22:42:39 +0800

    恢复误删除的内容->main.go”

--- main.go
+++ main.go
@@ -54,11 +54,11 @@ func main() {
 func run(ipChan chan<- *models.IP) {
 	var wg sync.WaitGroup
 	funs := []func() []*models.IP{
-		// getter.FQDL, //新代理
+		getter.FQDL, //新代理
 		//getter.Data5u,
 		//getter.Feiyi,
-		// getter.IP66, //need to remove it
-		// getter.IP3306,
+		getter.IP66, //need to remove it
+		getter.IP3306,
 		getter.KDL,
 		//getter.GBJ,	//因为网站限制，无法正常下载数据
 		//getter.Xici,
@@ -66,8 +66,8 @@ func run(ipChan chan<- *models.IP) {
 		//getter.IP181,  // 已经无法使用
 		//getter.YDL,	//失效的采集脚本，用作系统容错实验
 		// getter.PLP, //need to remove it
-		// getter.PLPSSL,
-		// getter.IP89,
+		getter.PLPSSL,
+		getter.IP89,
 	}
 	for _, f := range funs {
 		wg.Add(1)
