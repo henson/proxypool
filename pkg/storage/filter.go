eb462a772a348d349e4a75c250d27f86b512911d
walkskyer <walkskyer@qq.com>
2021-04-08 09:46:18 +0800

    err nil error
    err nil error

--- pkg/storage/filter.go
+++ pkg/storage/filter.go
@@ -106,10 +106,13 @@ func CheckProxyDB() {
 func ProxyRandom() (ip *models.IP) {
 
 	ips, err := models.GetAll()
+	if err != nil {
+		clog.Warn(err.Error())
+		return models.NewIP()
+	}
 	x := len(ips)
 	clog.Warn("len(ips) = %d", x)
-	if err != nil || x == 0 {
-		clog.Warn(err.Error())
+	if x == 0 {
 		return models.NewIP()
 	}
 	randomNum := RandInt(0, x)
